e972aaa7f2f8510bca09b35b7a25db4e667427fe
EnterpriseJavaProject <85710578+EnterpriseJavaProject@users.noreply.github.com>
2022-04-11 16:25:24 +0000

    updates

--- SQL_TABLES.sql
+++ SQL_TABLES.sql
@@ -1,115 +1,108 @@
-CREATE TABLE `users` (
-  `id` int AUTO_INCREMENT,
-  `usertype` varchar(255),
-  `email` varchar(255),
-  `password` varchar(255),
-  `created_at` timestamp,
-  `updated_at` timestamp,
-  PRIMARY KEY (`id`, `usertype`)
-);
+//// -- LEVEL 1
+//// -- Tables and References
 
-CREATE TABLE `students` (
-  `id` int AUTO_INCREMENT,
-  `users_id` int,
-  `student_id` varchar(255),
-  `full_name` varchar(255),
-  `date_of_birth` datetime,
-  `contact` varchar(255),
-  `gender` varchar(255),
-  `address` varchar(255),
-  `course_id` int,
-  `usertype` varchar(255),
-  `created_at` timestamp,
-  `updated_at` timestamp,
-   PRIMARY KEY (`id`, `student_id`)
-);
-
-CREATE TABLE `lectures` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `staff_id` varchar(255),
-  `users_id` int,
-  `full_name` varchar(255),
-  `contact` varchar(255),
-  `usertype` varchar(255),
-  `course_id` int,
-  `created_at` timestamp,
-  `updated_at` timestamp
-);
-
-CREATE TABLE `admin` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `full_name` varchar(255),
-  `usertype` varchar(255),
-  `users_id` int,
-  `created_at` timestamp,
-  `updated_at` timestamp
-);
-
-CREATE TABLE `courses` (
-  `id` int AUTO_INCREMENT,
-  `code` int,
-  `course_name` varchar(255),
-  `course_level` varchar(255),
-  `users_id` int,
-  `created_at` timestamp,
-  `updated_at` timestamp,
-  `course_start_date` datetime,
-  `certificate_issuedate` datetime,
-  PRIMARY KEY (`id`, `code`)
-);
-
-CREATE TABLE `results` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `course_id` int,
-  `module_id` int,
-  `student_id` int,
-  `grade` varchar(255),
-  `marks` int
-);
-
-CREATE TABLE `modules` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `module_name` varchar(255),
-  `module_code` varchar(255),
-  `created_at` timestamp,
-  `updated_at` timestamp
-);
-
-CREATE TABLE `add_module_to_course` (
-  `id` int PRIMARY KEY AUTO_INCREMENT,
-  `course_id` int,
-  `module_id` int,
-  `created_at` timestamp,
-  `updated_at` timestamp
-);
-
-ALTER TABLE `students` ADD FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);
-
-ALTER TABLE `lectures` ADD FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);
-
-ALTER TABLE `admin` ADD FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);
+// Creating tables
+Table courses  {
+  id int [pk, increment] // auto-increment
+  course_name varchar
+  course_level varchar
+  status varchar
+  created_at timestamp
+  updated_at timestamp
+  course_start_date date
+  course_end_date date
+  certificate_issuedate date
+}
 
 
 
-ALTER TABLE `students` ADD FOREIGN KEY (`usertype`) REFERENCES `users` (`usertype`);
+Table modules  {
+  id int [pk, increment] // auto-increment
+  module_name varchar
+  course_name varchar
+  staff_name varchar
+  course_id  int
+  status varchar
+  module_start_date date
+  module_end_date date
+  created_at timestamp
+  updated_at timestamp
+}
 
-ALTER TABLE `lectures` ADD FOREIGN KEY (`usertype`) REFERENCES `users` (`usertype`);
 
-ALTER TABLE `admin` ADD FOREIGN KEY (`usertype`) REFERENCES `users` (`usertype`);
+Table staffs  {
+  id int [pk, increment] // auto-increment
+  name varchar
+  staff_id varchar
+  email varchar
+  contact varchar
+  usertype varchar
+  course_id  int
+  status varchar
+  created_at timestamp
+  updated_at timestamp
+}
 
 
 
-ALTER TABLE `lectures` ADD FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`);
+Table students  {
+  id int [pk, increment] // auto-increment
+  name varchar
+  student_id  varchar
+  email varchar
+  contact varchar
+  date_of_birth varchar
+  gender varchar
+  fees varchar
+  course_id  int
+  status varchar
+  created_at timestamp
+  updated_at timestamp
+}
 
-ALTER TABLE `courses` ADD FOREIGN KEY (`users_id`) REFERENCES `users` (`id`);
 
-ALTER TABLE `results` ADD FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`);
 
-ALTER TABLE `results` ADD FOREIGN KEY (`student_id`) REFERENCES `students` (`id`);
+Table students_results  {
+  id int [pk, increment] // auto-increment
+  name varchar
+  student_id  varchar
+  course_id  int
+  module_id  int
+  module_name varchar
+  staff_name varchar
+  marks int
+  status varchar
+  created_at timestamp
+  updated_at timestamp
+}
 
-ALTER TABLE `add_module_to_course` ADD FOREIGN KEY (`course_id`) REFERENCES `courses` (`id`);
 
-ALTER TABLE `add_module_to_course` ADD FOREIGN KEY (`module_id`) REFERENCES `modules` (`id`);
 
-ALTER TABLE `modules` ADD FOREIGN KEY (`id`) REFERENCES `results` (`module_id`);
 
+
+Table users  {
+  id int [pk, increment] // auto-increment
+  staff_id varchar
+  email varchar
+  password varchar
+  usertype varchar
+  status varchar
+  created_at timestamp
+  updated_at timestamp
+}
+
+
+
+
+Ref: "courses"."id" < "modules"."course_id"
+
+
+Ref: "courses"."id" < "students_results"."course_id"
+
+Ref: "modules"."id" < "students_results"."module_id"
+
+Ref: "students"."student_id" < "students_results"."student_id"
+
+
+
+Ref: "staffs"."staff_id" < "users"."staff_id"
